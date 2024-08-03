079a7806826834f8c7846dc44607f6e184604687
Shubham <sjindal345@gmail.com>
2024-08-03 20:55:06 +0530

    MEDIA QUERIES

--- gdsc.js
+++ gdsc.js
@@ -1,21 +1,38 @@
-// Get modal elements
+/*Sign In*/ 
 const openModalButton = document.querySelector('.open-modal');
 const modalOverlay = document.querySelector('.modal-overlay');
 const closeModalButton = document.querySelector('.close-modal');
 
-// Open the modal
 openModalButton.addEventListener('click', () => {
     modalOverlay.style.display = 'flex';
 });
 
-// Close the modal
 closeModalButton.addEventListener('click', () => {
     modalOverlay.style.display = 'none';
 });
 
-// Close the modal if overlay is clicked
 modalOverlay.addEventListener('click', (e) => {
     if (e.target === modalOverlay) {
         modalOverlay.style.display = 'none';
     }
 });
+
+/*Join Us*/
+var modal = document.getElementById("joinUsModal");
+
+var btn = document.getElementById("joinUsId");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
