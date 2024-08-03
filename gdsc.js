fab931ac45b0420375f4bc2b259517f43d425616
Shubham <sjindal345@gmail.com>
2024-08-03 20:59:01 +0530

    gdsc interview

--- gdsc.js
+++ gdsc.js
@@ -31,6 +31,7 @@ btn.onclick = function() {
 span.onclick = function() {
     modal.style.display = "none";
 }
+
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
